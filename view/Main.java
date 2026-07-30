84137a16d7de3b5adf36c7f8190cc8777bee3a55
Pietro Lima <PietroLima135@gmail.com>
2026-07-29 22:04:45 -0300

    Add files via upload

---
+++ view/Main.java
@@ -0,0 +1,11 @@
+package projeto_integrador_java.view;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        new TelaInicial().setVisible(true);
+        
+    }
+    
+}
